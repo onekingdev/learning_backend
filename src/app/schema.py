374dd8877267922dfeed2fa102186d87bd6bbc5a
onekingdev <onekingdev@gmail.com>
2021-06-30 15:25:34 -0500

    Create GuardianStudentByGuardian resolver

--- src/app/schema.py
+++ src/app/schema.py
@@ -613,6 +613,8 @@ class Query(api.schema.Query, graphene.ObjectType):
     guardians_student = graphene.List(GuardianStudentSchema)
     guardian_student_by_id = graphene.Field(
         GuardianStudentSchema, id=graphene.String())
+    guardian_student_by_guardian = graphene.List(
+        GuardianStudentSchema, id=graphene.ID())
 
     def resolve_guardians_student(root, info, **kwargs):
         # Querying a list
@@ -621,6 +623,10 @@ class Query(api.schema.Query, graphene.ObjectType):
     def resolve_guardian_student_by_id(root, info, id):
         # Querying a single question
         return GuardianStudent.objects.get(pk=id)
+
+    def resolve_guardian_student_by_guardian(root, info, id):
+        # Querying a single question
+        return GuardianStudent.objects.filter(guardian=id)
 
     # ----------------- Achievement  ----------------- #
 
