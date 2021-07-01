b2cef426d443c7383538874fedbb6248a8edb240
onekingdev <onekingdev@gmail.com>
2021-07-01 01:02:29 -0500

    Reformat and remove unnecessary comments

--- src/audiences/admin.py
+++ src/audiences/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Audience
 from parler import admin as parler_admin
 
-# Register your models here.
+
 @admin.register(Audience)
 class AudienceAdmin(parler_admin.TranslatableAdmin):
     pass
