7a7259256a99e85df1e09cb275146883a00dd759
onekingdev <onekingdev@gmail.com>
2021-09-29 15:01:31 -0500

    wip, not working

--- src/kb/resources.py
+++ src/kb/resources.py
@@ -1,20 +1,15 @@
 from import_export.resources import ModelResource
 from import_export.fields import Field
 from .models import Topic
-from parler.utils.context import switch_language
+from app.resources import LangField
 
-
-class LangField(Field):
-    def clean(self, obj, data, is_m2m=False):
-        print('ahua', self, obj, data)
-        #with switch_language(obj, lang):
-        super().clean(obj, data, is_m2m=is_m2m)
-        
 
 class TopicAdminResource(ModelResource):
     language_code = Field(
+        attribute='get_current_language'
     )
     name = LangField(
+        attribute='name'
     )
 
     class Meta:
@@ -24,13 +19,6 @@ class TopicAdminResource(ModelResource):
         report_skipped = False
         exclude = ['parent']
 
-    def before_import_row(self, *args, **kwargs):
-        lang = args[0]['language_code']
-        self.LANG = lang
-        super().before_import_row(*args, **kwargs)
-
-    def before_save_instance(self, *args, **kwargs):
-        super().before_save_instance(*args, **kwargs)
 
 
 
