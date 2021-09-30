2a9fdbc96931f24d2ead95d589c20099baa754e7
onekingdev <onekingdev@gmail.com>
2021-09-29 20:15:35 -0500

    Delete useless files

--- src/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-
-
-app_name = 'api'
-
-urlpatterns = [
-    path('v1/', include('api.modules.v1.urls')),
-]
--- src/kb/serializers/areas_of_knowledge.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import AreaOfKnowledge
-
-class AreaOfKnowledgeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = AreaOfKnowledge
-        fields = ['id', 'name', 'hex_color']
