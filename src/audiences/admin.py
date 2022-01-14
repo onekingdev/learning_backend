dadb19dd7ed0621d3e2b7a77a4e4c3dfb8f32466
onekingdev <onekingdev@gmail.com>
2022-01-13 22:03:38 -0600

    Make audience admin import export

--- src/audiences/admin.py
+++ src/audiences/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Audience
 from parler import admin as parler_admin
+from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Audience)
-class AudienceAdmin(parler_admin.TranslatableAdmin):
+class AudienceAdmin(ImportExportModelAdmin, parler_admin.TranslatableAdmin):
     pass
