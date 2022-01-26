1bd773837596240dddd85ebf50a1b105a74b82a0
onekingdev <onekingdev@gmail.com>
2022-01-26 12:49:12 -0600

    Add stripe url

--- src/app/urls.py
+++ src/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from users.admin import hidden_admin
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hidden-admin/', hidden_admin.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('stripe/', csrf_exempt(include("djstripe.urls", namespace="djstripe"))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
