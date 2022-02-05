40022484be462ab37ada497fe53d3050cdc7405c
onekingdev <onekingdev@gmail.com>
2022-02-06 03:24:29 +0500

    fix wallet query

--- src/wallets/schema.py
+++ src/wallets/schema.py
@@ -15,7 +15,7 @@ class Query(graphene.ObjectType):
     coin_wallet = graphene.List(CoinWalletSchema)
     coin_wallet_by_id = graphene.Field(CoinWalletSchema, id=graphene.ID())
 
-    def resolve_coin_wallets(root, info, **kwargs):
+    def resolve_coin_wallet(root, info, **kwargs):
         # Querying a list
         return CoinWallet.objects.all()
 
