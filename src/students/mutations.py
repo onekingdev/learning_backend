8384a9cc4c04608b0e67c5f8600ef9203bd3b6cd
onekingdev <onekingdev@gmail.com>
2022-02-09 01:46:11 +0000

    Change typo of user to users

--- src/students/mutations.py
+++ src/students/mutations.py
@@ -1,8 +1,5 @@
-import os
-import sys
 import graphene
 from django.contrib.auth import get_user_model
-from django.db import transaction, DatabaseError
 from api.models import profile
 from graphql_jwt.shortcuts import create_refresh_token, get_token
 from .models import Student
@@ -44,147 +41,67 @@ class CreateStudent(graphene.Mutation):
             dob,
             student_plan):
 
-        try:
-            with transaction.atomic():
-                user = get_user_model()()
-                student = Student(
-                    first_name=first_name,
-                    last_name=last_name,
-                    full_name=first_name + ' ' + last_name
-                )
+        user = get_user_model()()
+        student = Student(
+            first_name=first_name,
+            last_name=last_name,
+            full_name=first_name + ' ' + last_name
+        )
 
-                if username:
-                    user.username = username
+        if username:
+            user.username = username
+        else:
+            count = 1
+            while True:
+                if get_user_model().objects.get(username=username).exists():
+                    count += 1
                 else:
-                    count = 1
-                    while True:
-                        if get_user_model().objects.get(username=username).exists():
-                            count += 1
-                        else:
-                            break
-                    user.username = first_name + last_name + count
+                    break
+            user.username = first_name + last_name + count
 
-                if password:
-                    user.set_password(password)
-                else:
-                    user.set_unusable_password()
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
 
-                user.save()
+        user.save()
 
-                student.user = user
+        student.user = user
 
-                if dob:
-                    student.dob = dob
+        if dob:
+            student.dob = dob
 
-                student.save()
+        # Student Plan
+        if student_plan:
+            pass
+        elif school:
+            student_plan = School.objects.get(school).student_plan
+        elif group:
+            audience = Group.objects.get(group).audience
+            student_plan = Audience.objects.get(audience).student_plan
+        elif grade:
+            audience = Grade.objects.get(grade).audience
+            student_plan = Audience.objects.get(audience).student_plan
+        else:
+            student_plan = StudentPlan.objects.get_or_create(
+                name='Default Plan')
 
-                # Student Plan
-                if student_plan:
-                    pass
-                elif school:
-                    student_plan = School.objects.get(school).student_plan
-                elif group:
-                    audience = Group.objects.get(group).audience
-                    student_plan = Audience.objects.get(audience).student_plan
-                elif grade:
-                    audience = Grade.objects.get(grade).audience
-                    student_plan = Audience.objects.get(audience).student_plan
-                else:
-                    student_plan = StudentPlan.objects.get_or_create(
-                        name='Default Plan')
+        student.student_plan = StudentPlan.objects.get(student_plan)
 
-                student_plan = StudentPlan.objects.get(pk=student_plan)
-                student.student_plan.add(student_plan)
+        student.save()
 
-                student.save()
+        profile_obj = profile.objects.get(user=user.id)
+        token = get_token(user)
+        refresh_token = create_refresh_token(user)
 
-                profile_obj = profile.objects.get(user=user.id)
-                token = get_token(user)
-                refresh_token = create_refresh_token(user)
-
-                return CreateStudent(
-                    student=student,
-                    user=user,
-                    profile=profile_obj,
-                    token=token,
-                    refresh_token=refresh_token
-                )
-        except (Exception, DatabaseError) as e:
-            transaction.rollback()
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno)
-            return e
-
-
-class ChangeStudentPassword(graphene.Mutation):
-    student = graphene.Field('students.schema.StudentSchema')
-    user = graphene.Field(UserSchema)
-    profile = graphene.Field(UserProfileSchema)
-
-    class Arguments:
-        student_id = graphene.ID(required=True)
-        password = graphene.String(required=True)
-
-    def mutate(
-            self,
-            info,
-            student_id,
-            password):
-        try:
-            with transaction.atomic():
-                student = Student.objects.get(pk=student_id)
-                student.user.set_password(password)
-                student.user.save()
-
-                profile_obj = profile.objects.get(user=student.user.id)
-
-                return ChangeStudentPassword(
-                    student=student,
-                    user=student.user,
-                    profile=profile_obj,
-                )
-        except (Exception, DatabaseError) as e:
-            transaction.rollback()
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno)
-            return e
-
-
-# class ChangeStudentGrade(graphene.Mutation):
-#     student = graphene.Field('students.schema.StudentSchema')
-#
-#     class Arguments:
-#         student_id = graphene.ID(required=True)
-#         grade_id = graphene.ID(required=True)
-#
-#     def mutate(
-#             self,
-#             info,
-#             student_id,
-#             password):
-#         try:
-#             with transaction.atomic():
-#                 student = Student.objects.get(pk=student_id)
-#                 student.user.set_password(password)
-#                 student.user.save()
-#
-#                 profile_obj = profile.objects.get(user=student.user.id)
-#
-#                 return ChangeStudentPassword(
-#                     student=student,
-#                     user=student.user,
-#                     profile=profile_obj,
-#                 )
-#         except (Exception, DatabaseError) as e:
-#             transaction.rollback()
-#             exc_type, exc_obj, exc_tb = sys.exc_info()
-#             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-#             print(exc_type, fname, exc_tb.tb_lineno)
-#             return e
+        return CreateStudent(
+            student=student,
+            user=user,
+            profile=profile_obj,
+            token=token,
+            refresh_token=refresh_token
+        )
 
 
 class Mutation(graphene.ObjectType):
     create_student = CreateStudent.Field()
-    change_student_password = ChangeStudentPassword.Field()
--- src/users/schema.py
+++ src/users/schema.py
@@ -1,8 +1,6 @@
-import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 from api.models import profile
-from django.contrib.auth.models import User
 
 
 class UserSchema(DjangoObjectType):
@@ -13,19 +11,3 @@ class UserSchema(DjangoObjectType):
 class UserProfileSchema(DjangoObjectType):
     class Meta:
         model = profile
-
-
-class Query(graphene.ObjectType):
-
-    # ----------------- User ----------------- #
-
-    users = graphene.List(UserSchema)
-    user_by_id = graphene.Field(UserSchema, id=graphene.ID())
-
-    def resolve_users(root, info, **kwargs):
-        # Querying a list
-        return User.objects.all()
-
-    def resolve_user_by_id(root, info, id):
-        # Querying a single question
-        return User.objects.get(pk=id)
