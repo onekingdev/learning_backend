f0ba5a954e52d80a740da18ceaed1066cfa005ac
onekingdev <onekingdev@gmail.com>
2022-03-29 23:43:37 +0000

    Add filter to TopicMasterySettings

--- src/engine/admin.py
+++ src/engine/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TopicMasterySettings
 
 # Register your models here.
+@admin.register(TopicMasterySettings)
+class TopicMasterySettingsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'topic',
+        'sample_size',
+        'mastery_percentage',
+        'competence_percentage',
+    )
+    list_filter = (
+        'topic__area_of_knowledge__universal_area_knowledge',
+    )
