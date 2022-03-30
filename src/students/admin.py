34c9727278c9d4c0eac164013fe1dc53103602d6
onekingdev <onekingdev@gmail.com>
2022-03-30 06:01:45 +0000

    Create Student admin

---
+++ src/students/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from admin_auto_filters.filters import AutocompleteFilter
+from .models import Student, StudentTopicStatus, StudentTopicMastery
+
+
+class StudentFilter(AutocompleteFilter):
+    title = 'Student'
+    field_name = 'student'
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['user']
+
+
+@admin.register(StudentTopicStatus)
+class StudentTopicStatus(admin.ModelAdmin):
+    search_fields = [
+        'topic__translations__name',
+        'student__user__username',
+    ]
+    list_filter = (
+        'student',
+        'topic__area_of_knowledge__universal_area_knowledge',
+    )
+    list_display = (
+        'id',
+        'update_timestamp',
+        'student',
+        'topic',
+        'status',
+    )
+
+
+@admin.register(StudentTopicMastery)
+class StudentTopicMastery(admin.ModelAdmin):
+    search_fields = [
+        'topic__translations__name',
+        'student__user__username',
+    ]
+    list_filter = (
+        'student',
+        'topic__area_of_knowledge__universal_area_knowledge',
+    )
+    list_display = (
+        'id',
+        'update_timestamp',
+        'student',
+        'topic',
+        'mastery_level',
+    )
