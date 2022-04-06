f6c00951af83f6ca3c86508cdfb8fba734f2ab97
onekingdev <onekingdev@gmail.com>
2022-04-06 23:56:17 +0000

    Disable topics without parent on block presentation creation

--- src/block/mutations.py
+++ src/block/mutations.py
@@ -90,7 +90,7 @@ class CreateAIBlockPresentation(graphene.Mutation):
         except AreaOfKnowledge.DoesNotExist:
             raise Exception("Area of knowledge does not exist")
 
-        topics = area_of_knowledge.topic_set.all()
+        topics = area_of_knowledge.topic_set.all().filter(parent=None)
 
         qs1 = StudentTopicStatus.objects.filter(
             student=student,
