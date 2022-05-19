58866924e7a22b240f2dfa614edae4ca3fec115d
onekingdev <onekingdev@gmail.com>
2022-05-18 17:32:12 -0700

    Stripe web hook (#136)

--- src/payments/views.py
+++ src/payments/views.py
@@ -49,44 +49,25 @@ def stripeWebHook(request):
                 try :
                     subscription_status = subscription['status']
                     subscription_end_at = datetime.datetime.fromtimestamp(subscription['current_period_end'])
-                    print(subscription['current_period_end'])
                     # ---------- Get guardian student plan from subscription id -S----------------#
                     order_detail = OrderDetail.objects.get(subscription_id = subscription['id'])
-                    # order_detail = OrderDetail.objects.get(subscription_id = "sub_1KqOQNAfwM01sssKrJfXOzrb")
-                    # order_detail = OrderDetail.objects.get(pk=112)
-                    # if(not order_detail): 
-                    #     # --------------- Get user by email from request -S---------------------#
-                    #     user = User.objects.filter(email = intent.charges.data[0].billing_details.email)
-                    #     if(len(user) < 1) :
-                    #         return JsonResponse({"status": "error", "message": "Current user was deleted on database" })
-                    #     user = user[0]
-                    #     guardian_student_plans = user.guardian.guardianstudentplan_set.all()
-                    #     # --------------- Get user by email from request -E---------------------#
-                    # # order_detail = order_detail[0]
-                    # else:
-                    #     guardian_student_plans = order_detail.guardianstudentplan_set.all()
                     guardian_student_plans = order_detail.guardianstudentplan_set.all()
-                    print(guardian_student_plans, guardian_student_plans[0])
                     # ---------- Get guardian student plan from subscription id -E----------------#
-
-                    # guardian = user.guardian
-                    # guardianStudentPlans = guardian.guardianstudentplan_set
 
                     # --------------- Update Expire Date to after period days -S----------------- #
                     for guardian_student_plan in guardian_student_plans:
-                        # period = 32 if guardian_student_plan.period == "MONTHLY" else 367
                         period =2
                         guardian_student_plan.is_paid = True if subscription_status == "active" or subscription_status == "incomplete" else False
                         guardian_student_plan.expired_at = subscription_end_at + datetime.timedelta(days=period)
                         guardian_student_plan.save()
 
                         guardian_student_plan.order_detail.is_paid = True if subscription_status == "active" or subscription_status == "incomplete" else False
-                        guardian_student_plan.order_detail.expired_at = now + datetime.timedelta(days=period)
+                        guardian_student_plan.order_detail.expired_at = subscription_end_at + datetime.timedelta(days=period)
                         guardian_student_plan.order_detail.save()
                     # --------------- Update Expire Date to after period days -E----------------- #
                 except Exception as e:
                     response_err.append("Subscription ID " + subscription['id'] + " : " + str(e))
-            return JsonResponse({"status": "failed", "msg": response_err}) if len(response_err) > 0 else JsonResponse({"status": "success"})
+            return JsonResponse({"status": "Warning", "msg": response_err}) if len(response_err) > 0 else JsonResponse({"status": "success"})
         # -------------- If payment is succeeded on stripe -S----------------#
 
         # -------------- If payment is failed on stripe -S----------------#
@@ -96,39 +77,29 @@ def stripeWebHook(request):
             subscriptions = stripe.Subscription.list(customer=customer_id)
 
             for subscription in subscriptions:
-                subscription_status = subscription.data.status
-                subscription_end_at = datetime.datetime.fromtimestamp(subscription.data.current_period_end)
-                # ---------- Get guardian student plan from subscription id -S----------------#
-                order_detail = OrderDetail.objects.filter(subscription_id = subscription.data.id)
-                order_detail = order_detail[0]
-                guardian_student_plans = order_detail.guardianstudentplan_set
-                # ---------- Get guardian student plan from subscription id -E----------------#
+                try :
+                    subscription_status = subscription['status']
+                    subscription_end_at = datetime.datetime.fromtimestamp(subscription['current_period_end'])
+                    # ---------- Get guardian student plan from subscription id -S----------------#
+                    order_detail = OrderDetail.objects.get(subscription_id = subscription['id'])
+                    guardian_student_plans = order_detail.guardianstudentplan_set.all()
+                    # ---------- Get guardian student plan from subscription id -E----------------#
 
-                # --------------- Get user by email from requedst -S---------------------#
-                # user = User.objects.filter(email = intent.charges.data[0].billing_details.email)
-                # if(len(user) < 1) :
-                #     return JsonResponse({"status": "error", "message": "Current user was deleted on database" })
-                # user = user[0]
-                # --------------- Get user by email from request -E---------------------#
+                    # --------------- Update Expire Date to after period days -S----------------- #
+                    for guardian_student_plan in guardian_student_plans:
+                        period =2
+                        guardian_student_plan.is_paid = False
+                        guardian_student_plan.expired_at = subscription_end_at
+                        guardian_student_plan.save()
 
-                # guardian = user.guardian
-                # guardianStudentPlans = guardian.guardianstudentplan_set
-
-                # --------------- Update Expire Date to after period days -S----------------- #
-                for i, guardian_student_plan in guardian_student_plans:
-                    # period = 32 if guardian_student_plans[i].period == "MONTHLY" else 367
-                    period =2
-
-                    guardian_student_plans[i].is_paid = True if subscription_status == "active" or subscription_status == "incomplete" else False
-                    guardian_student_plans[i].expired_at = subscription_end_at + datetime.timedelta(days=period)
-                    guardian_student_plans[i].save()
-
-                    guardian_student_plans[i].order_detail.is_paid = True if subscription_status == "active" or subscription_status == "incomplete" else False
-                    guardian_student_plans[i].order_detail.expiredAt = now + datetime.timedelta(days=period)
-                    guardian_student_plans[i].order_detail.save()
-                # --------------- Update Expire Date to after period days -E----------------- #
-            
-            return JsonResponse({"status": "success"})
+                        guardian_student_plan.order_detail.is_paid = False
+                        guardian_student_plan.order_detail.expired_at = subscription_end_at
+                        guardian_student_plan.order_detail.save()
+                    # --------------- Update Expire Date to after period days -E----------------- #
+                except Exception as e:
+                    response_err.append("Subscription ID " + subscription['id'] + " : " + str(e))
+            return JsonResponse({"status": "Warning", "msg": response_err}) if len(response_err) > 0 else JsonResponse({"status": "success"})
+        
         # -------------- If payment is failed on stripe -S----------------#
         else:
             return JsonResponse({"status": "failed", "msg": "unknown events"})
