12b8f7d34e3d542dc60f767a5b2fc43493923dc1
onekingdev <onekingdev@gmail.com>
2022-05-30 23:03:53 +0200

    change background image of django

--- src/app/backends.py
+++ src/app/backends.py
@@ -5,6 +5,7 @@ from django.contrib.auth.backends import ModelBackend
 class CustomBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
+        print("authenticate")
         if username is not None:
             UserModel = get_user_model()
 
