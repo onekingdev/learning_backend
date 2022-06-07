13eee96187876443cace42e5697c810c5273266f
onekingdev <onekingdev@gmail.com>
2022-06-08 05:05:20 +0700

    Refactored BlockQuestionPresentationHistoryByStudentID Resolver #2 (#165)

--- src/block/schema.py
+++ src/block/schema.py
@@ -225,7 +225,7 @@ class Query(graphene.ObjectType):
         return StudentBlockQuestionPresentationHistory.objects.get(pk=id);
 
     def resolve_block_question_presentation_history_by_student_id(root, info, id):
-        return StudentBlockQuestionPresentationHistory.objects.get(student=id);
+        return StudentBlockQuestionPresentationHistory.objects.filter(student=id);
 
     # ----------------- BlockAssignment ----------------- #
 
