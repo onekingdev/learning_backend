dfc38df2774321e74d594cf2970117331a4d0b48
onekingdev <onekingdev@gmail.com>
2022-07-14 21:48:48 +0200

    note admin

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from notes.models import Notes
+from import_export import admin as import_export_admin
+
+@admin.register(Notes)
+class NotesAdmin(
+        import_export_admin.ImportExportModelAdmin,
+):
+    list_display = (
+            'title',
+            'user_from',
+            'user_to',
+            'send_at'
+    )
 # Register your models here.
