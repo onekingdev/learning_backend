94d7c78b2445dad6fc6e8eb25f37ce669d3033d3
onekingdev <onekingdev@gmail.com>
2022-07-18 20:40:09 +0200

    payment admin

--- src/payments/admin.py
+++ src/payments/admin.py
@@ -9,13 +9,40 @@ class PaymentMethodAdmin(
 ):
     list_display = (
             'id',
-            'guardian',
+            'school',
             'teacher',
+            'guardian',
             'card',
+            'card_first_name',
+            'card_last_name',
+            'card_exp_year',
+            'card_exp_month',
+            'card_cvc',
             'country',
+            'state',
+            'city',
+            'post_code',
             'phone',
             'is_default',
     )
+    search_fields = [
+            'school__name',
+            'teacher__user__username',
+            'guardian__user__username',
+            'card_number',
+            'card_first_name',
+            'card_last_name',
+            'card_exp_year',
+            'card_exp_month',
+            'card_cvc',
+            'country',
+            'state',
+            'city',
+            'post_code',
+            'phone',
+    ]
+    list_filter = ('create_timestamp', 'update_timestamp', 'is_default', 'country')
+
     @admin.display(description='Card Number')
     def card(self, obj):
         str = None
@@ -30,24 +57,49 @@ class PaymentMethodAdmin(
 class CardTransactionAdmin(
         import_export_admin.ImportExportModelAdmin,
 ):
-    list_display = (
-            'id',
-            'order_detail',
-            'session_id',
-            'card',
-            'country',
-            'phone',
-            'is_captured'
-    )
-    @admin.display(description='Card Number')
-    def card(self, obj):
-        str = None
-        if(obj.card_number):
-            card_leng = len(obj.card_number)
-            first_4 = obj.card_number[0:4]
-            last_4 = obj.card_number[card_leng-4:card_leng]
-            str = f"{first_4} *** {last_4}"
-        return str
+        list_display = (
+                'id',
+                'order_detail',
+                'session_id',
+                'card',
+                'card_exp_year',
+                'card_exp_month',
+                'card_cvc',
+                'country',
+                'city',
+                'state',
+                'post_code',
+                'phone',
+                'is_captured'
+        )
+        search_fields = [
+                'id',
+                'order_detail__id',
+                'session_id',
+                'card_number',
+                'card_first_name',
+                'card_last_name',
+                'card_exp_year',
+                'card_exp_month',
+                'card_cvc',
+                'country',
+                'city',
+                'state',
+                'post_code',
+                'phone',
+                'is_captured'
+        ]
+        list_filter = ('create_timestamp', 'update_timestamp', 'country', 'is_captured')
+
+        @admin.display(description='Card Number')
+        def card(self, obj):
+                str = None
+                if(obj.card_number):
+                        card_leng = len(obj.card_number)
+                        first_4 = obj.card_number[0:4]
+                        last_4 = obj.card_number[card_leng-4:card_leng]
+                        str = f"{first_4} *** {last_4}"
+                return str
 
 class ReadOnlyDiscountCodeForm(ModelForm):
     class Meta:
@@ -81,18 +133,84 @@ class OrderDetailAdmin(
         import_export_admin.ImportExportModelAdmin,
 ):
     list_display = (
-            'id',
-            'order',
-            'plan',
-            'payment_method_plan_id',
-            'subscription_id',
-            'quantity',
-            'period',
-            'discount',
-            'total',
-            'is_paid',
-            'is_cancel',
+        'id',
+        'order',
+        'school',
+        'teacher',
+        'guardian',
+        'plan',
+        'payment_method_plan_id',
+        'quantity',
+        'period',
+        'discount',
+        'total',
+        'discount_code',
+        'payment_method',
+        'expired_at',
+        'is_paid',
+        'is_cancel',
+        'on_discount',
     )
+    list_filter = (
+        'plan',
+        'order__discount_code',
+        'order__payment_method',
+        'period',
+        'is_paid',
+        'is_cancel',
+        'on_discount',
+        'create_timestamp',
+        'update_timestamp',
+        )
+
+    search_fields = [
+        'id',
+        'order__id',
+        'order__guardian__user__username',
+        'order__school__name',
+        'order__teacher__user__username',
+        'order__discount_code',
+        'order__payment_method',
+        'plan__name',
+        'payment_method_plan_id',
+        'subscription_id',
+        'quantity',
+        'period',
+        'discount',
+        'total',
+        'expired_at',
+        'status',
+        ]
+    search_help_text = "Search by id, order_id, order_guardian, order_school, order_teacher, discount code, plan_name, payment_method_plan_id, subscription_id, quantity, period, discount, total, expired_at, status"
+    
+    @admin.display(description='Teacher', ordering='order__teacher')
+    def teacher(self, obj):
+        if obj.order:
+                return obj.order.teacher
+        return None
+
+    @admin.display(description='Guardian', ordering='order__guardian')
+    def guardian(self, obj):
+        if obj.order:
+                return obj.order.guardian
+        return None
+
+    @admin.display(description='School', ordering='order__school')
+    def school(self, obj):
+        if obj.order:
+                return obj.order.school
+        return None
+    @admin.display(description='Discount Code', ordering='order__discount_code')
+    def discount_code(self, obj):
+        if obj.order:
+                return obj.order.discount_code
+        return None
+    @admin.display(description='Payment Method', ordering='order__payment_method')
+    def payment_method(self, obj):
+        if obj.order:
+                return obj.order.payment_method
+        return None
+
 
 @admin.register(Order)
 class OrderAdmin(
@@ -108,6 +226,31 @@ class OrderAdmin(
             'payment_method',
             'is_paid',
     )
+    list_filter = (
+        'discount_code',
+        'payment_method',
+        'is_paid',
+        'create_timestamp',
+        'update_timestamp',
+        )
+
+    search_fields = [
+        'id',
+        'order__id',
+        'order__guardian__user__username',
+        'order__school__name',
+        'order__teacher__user__username',
+        'order__discount_code',
+        'plan__name',
+        'payment_method_plan_id',
+        'subscription_id',
+        'quantity',
+        'period',
+        'discount',
+        'total',
+        'expired_at',
+        'status',
+        ]
 
 @admin.register(PaymentHistory)
 class PaymentHistoryAdmin(
@@ -130,9 +273,9 @@ class PaymentHistoryAdmin(
         #--------------- ADD Date Hierarcy ---------------------#
         date_hierarchy = 'create_timestamp'
         #--------------- ADD Search Field ----------------------#
-        search_fields = ['user__email', 'card_number', 'type']
+        search_fields = ['user__email', 'card_number', 'type', 'order__id']
         #---------------#
-        search_help_text = "Search by email, card number and type"
+        search_help_text = "Search by email, card number, order_id and type"
 
         @admin.display(description='Card Number', ordering='card_number')
         def card(self, obj):
