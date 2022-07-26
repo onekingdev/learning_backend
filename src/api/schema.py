003b34f51035e368b5f180e4be1ceaf25fcadd36
onekingdev <onekingdev@gmail.com>
2022-07-26 20:38:36 +0200

    update createGuardian for firstname and last name

--- src/api/schema.py
+++ src/api/schema.py
@@ -84,6 +84,8 @@ class CreateGuardian(graphene.Mutation):
                 user = get_user_model()(
                     username=username,
                     language=language,
+                    first_name = first_name,
+                    last_name = last_name,
                 )
                 user.set_password(password)
                 if email is not None:
